060780475d106f553b765136e831069631d2ae5f
soood <soood_al3ion@yahoo.com>
2017-10-21 04:09:35 +0300

    Create search.js

---
+++ search.js
@@ -0,0 +1,18 @@
+<!-- google custom search by bohaish start -->
+<script src='//www.google.com/jsapi' type='text/javascript'></script>
+<script type='text/javascript'>
+google.load('search', '1', {language: 'ar', style: google.loader.themes.SHINY});
+google.setOnLoadCallback(function() {
+  var customSearchOptions = {};
+  var orderByOptions = {};
+  orderByOptions['keys'] = [{label: 'العلاقة', key: ''} , {label: 'التاريخ', key: 'date'}];
+  customSearchOptions['enableOrderBy'] = true;
+  customSearchOptions['orderByOptions'] = orderByOptions;
+  var customSearchControl =   new google.search.CustomSearchControl('017205871479523372903:suztx4svkuq', customSearchOptions);
+  customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+  var options = new google.search.DrawOptions();
+  options.setAutoComplete(true);
+  customSearchControl.draw('cse', options);
+}, true);
+</script>
+<!-- google custom search by bohaish end -->
